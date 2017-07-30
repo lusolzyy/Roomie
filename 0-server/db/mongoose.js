9474e6f5d0057e35fb0af4588018f021a7522a52
lusolzyy <lusolzyy@gmail.com>
2017-07-30 06:46:26 -0700

    Update mongoose.js

--- 0-server/db/mongoose.js
+++ 0-server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/Roomie');
-mongoose.connect('mongodb://lusolzyy:Lusolzyy1993@ds157040.mlab.com:57040/roommate');
+mongoose.connect('mongodb://localhost:27017/Roomie');
+
 
 module.exports =  {
   mongoose
